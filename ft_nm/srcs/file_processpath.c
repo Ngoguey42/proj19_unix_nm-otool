454cb7b9400c67c15c991cf1ced18bda43ca5a6b
Ngoguey42 <ngoguey@student.42.fr>
2016-02-08 19:37:30 +0100

    reverted change in srcs/file_processpath.c

--- ft_nm/srcs/file_processpath.c
+++ ft_nm/srcs/file_processpath.c
@@ -6,7 +6,7 @@
 /*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 16:12:49 by ngoguey           #+#    #+#             */
-/*   Updated: 2016/02/07 16:12:49 by ngoguey          ###   ########.fr       */
+/*   Updated: 2016/02/08 19:36:28 by ngoguey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ t_filename		nm_file_make_processpath(char const *file)
 	size_t const		strlen = ft_strlen(file);
 	size_t				filelen;
 
-	if (strlen == 0 || file[strlen - 1] != ')' || opparen == NULL)
+	if (strlen == 0 || file[strlen - 1] != ')' || opparen == NULL
+		|| opparen == file)
 		return ((t_filename){file, NULL, strlen, 0});
 	filelen = opparen - file;
 	return ((t_filename){file, opparen + 1,
